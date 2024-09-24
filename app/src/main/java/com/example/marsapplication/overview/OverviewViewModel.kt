1b9d34870ea705eea5c6325e52e8ad0fc29af69d
ebruaeo <ebruaeo@hotmail.com>
2024-09-24 18:24:49 +0300

    Connecting to the Internet and Parsing the JSON Response

--- app/src/main/java/com/example/marsapplication/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/marsapplication/overview/OverviewViewModel.kt
@@ -3,27 +3,36 @@ package com.example.marsapplication.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.marsapplication.network.MarsApi
+import com.example.marsapplication.network.MarsProperty
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
 
-    // The external immutable LiveData for the request status String
+    private val _response = MutableLiveData<String>()
     val response: LiveData<String>
         get() = _response
 
-    /**
-     * Call getMarsRealEstateProperties() on init so we can display status immediately.
-     */
+
     init {
         getMarsRealEstateProperties()
     }
 
-    /**
-     * Sets the value of the status LiveData to the Mars API status.
-     */
+
     private fun getMarsRealEstateProperties() {
+        MarsApi.retrofitService.getProperties().enqueue(object : Callback<List<MarsProperty>>{
+            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+                _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
+            }
+
+            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
+               _response.value = "Failure: " + t.message
+            }
+
+        })
         _response.value = "Set the Mars API Response here!"
     }
 }
